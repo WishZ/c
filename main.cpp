1f7294f1cb4b43f8abd0050b4f75c784ab38a9d8
sunshine <wishzhang77@gmail.com>
2019-12-19 21:51:35 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,9 @@
 #include<cstdio>
 
 int main() {
-//需要一个把英寸单位转换为厘米单位（1 英寸=2.54 厘米）的程序。该程序要提示用户输入英寸值
-    double inch;
-
-    printf("单位换算：英寸转厘米，请输入英寸值\n");
-
-    scanf("%lf", &inch);
-
-    printf("%f 英寸等于 %lf 厘米!\n",inch, inch * 2.54);
+    int ch ;
+    while ((ch = getchar()) != EOF)
+        putchar(ch);
 
     return 0;
-
 }
