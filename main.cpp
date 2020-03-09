0a6dbe20e652600358c6fa8f14cf4b9d5c59e1dd
sunshine <1130018964@qq.com>
2020-03-09 21:47:50 +0800

    结合体  联合体

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ public:
         y = yy;
     }
 
+    ~Point() {
+        cout << "析构函数";
+    }
+
     Point(Point &p);
 
     int getX() {
@@ -59,7 +63,70 @@ Line::Line(Line &l) : p1(l.p1), p2(l.p2) {//组合类的复制构造函数
     len = l.len;
 }
 
+//结合体
+struct Student {
+    int age;
+    string name;
+    char sex;
+    int num;
+};
+//联合体  成员公用同一组内存单元  任何两个成员不会同时有效
+
+class ExamInfo {
+private:
+    string name;
+    enum {
+        GRADE, PASS, PERCENTAGE
+    } mode;
+    union {
+        char grade;
+        bool pass;
+        int percent;
+    };
+public:
+    ExamInfo(string name, char grade) : name(name), mode(GRADE), grade(grade) {}
+
+    ExamInfo(string name, bool pass) : name(name), mode(PASS), pass(pass) {}
+
+    ExamInfo(string name, int percent) : name(name), mode(PERCENTAGE), percent(percent) {}
+
+    void show();
+};
+
+void ExamInfo::show() {
+    cout << name << ":";
+    switch (mode) {
+        case GRADE:
+            cout << grade;
+            break;
+        case PASS:
+            cout << (pass ? "PASS" : "FAIL");
+            break;
+        case PERCENTAGE:
+            cout << percent;
+            break;
+    }
+    cout << endl;
+}
+
+enum class Side {
+    Right, left
+};
+enum class Thing {
+    Wrong, Right
+};
+
 int main() {
+    Side s = Side::Right;
+    Thing t = Thing::Right;
+    ExamInfo course1("英语", 'B');
+    ExamInfo course2("数学", true);
+    ExamInfo course3("C++", 85);
+    course1.show();
+    course2.show();
+    course3.show();
+    Student stu = {19, "张三", 'F', 22};
+    cout << stu.age << endl;
     Point myP1(1, 1), myP2(4, 5);
     Line line(myP1, myP2);
     Line line2(line);
